158f5b69a50550bb55f75babf23a2d34b1ecbd0b
Jesse Hockenbury <hockenburj1@nku.edu>
2016-04-19 20:34:56 -0400

    link logo to homepage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $( document ).ready( function() {
   $( '.rslides' ).responsiveSlides();
+
+    map = new GMaps( {
+        div: '#map',
+        lat: 28.371099,
+        lng: -81.517777
+    } );
+
+    map.addMarker( {
+        lat: 28.371099,
+        lng: -81.517777,
+        title: 'Pizza Planet'
+    } );
 } );
